435765f9538d553242df870705e859a71b4a5468
Thiagod3 <thiago.vilela3@outlook.com>
2023-07-16 00:07:36 -0300

    feat: tratamento cidade inexistente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState } from "react";
 import "./styles.css";
 
 const apiurl =
@@ -9,21 +9,28 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useState("");
 
-//Usa a API para buscar a cidade e, se positivo, a adiciona à "previsao"
+  const [erroVisivel, setErroVisivel] = useState(false);
+
+  //Usa a API para buscar a cidade e, se positivo, a adiciona à "previsao"
   const handleSearch = () => {
     fetch(`${apiurl}${searchTerm}&lang=pt`)
       .then((response) => {
         if (response.status === 200) {
           return response.json();
+        } else {
+          throw new Error("Erro na requisição");
         }
       })
       .then((data) => {
+        setErroVisivel(false);
         setPrevisao(data);
-      });
+      })
+      .catch((error) => {
+            setErroVisivel(true);});
   };
 
-//chama "handleSearch" ao pressionar Enter na busca
-    const handleKeyPress = (event) => {
+  //chama "handleSearch" ao pressionar Enter na busca
+  const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       handleSearch();
     }
@@ -31,37 +38,35 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div>
         {previsao ? (
           <div className="Resultado">
-            <h1>Clima em {previsao.location.name}</h1>
+            <header>
+              <i class="gg-arrow-left" onClick={() => setPrevisao(null)}></i>
+              <h2>{previsao.location.name}</h2>
+            </header>
             <div className="graus">
               <p id="temp">{previsao.current.temp_c}°C</p>
               <img src={previsao.current.condition.icon} />
             </div>
             <p id="cond">{previsao.current.condition.text}</p>
-            <input
-              placeholder="Buscar cidade..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-                onKeyDown={handleKeyPress}
-            />
-
-            <button onClick={handleSearch}>Procurar</button>
           </div>
         ) : (
-          <div className="Interface">
-            <h1>Site Clima</h1>
+          <div className="container">
+            {erroVisivel && <p id="erro">Cidade não encontrada!</p>}
+            <div className="Interface">
+              <h1>Site Clima</h1>
 
-            <p id="info">Digite sua cidade:</p>
+              <p id="info">Digite sua cidade:</p>
 
-            <input
-              placeholder="Buscar cidade..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
+              <input
+                placeholder="Buscar cidade..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
 
-            <button onClick={handleSearch}>Procurar</button>
+              <button onClick={handleSearch}>Procurar</button>
+            </div>
           </div>
         )}
       </div>
